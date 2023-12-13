78c044cf2909052361231d7cc30d81ca9608aaf7
chiragpdudhat <chiragpdudhat@gmail.com>
2023-12-13 11:35:30 +0530

    chage aboutus

--- src/component/Aboutus.jsx
+++ src/component/Aboutus.jsx
@@ -5,130 +5,89 @@ const Aboutus = () => {
     color: "black",
     backgroundColor: "white",
   });
-  const [btntext, setBtnText] = useState("Enable light Mode");
-
-  const togglestyle = () => {
-    if (navbg.color === "black") {
-      setMyStyle({
-        color: "white",
-        backgroundColor: "black",
-      });
-      setBtnText("Enable Dark  Mode");
-    } else {
-      setMyStyle({
-        color: "black",
-        backgroundColor: "white",
-      });
-      setBtnText("Enable light Mode");
-    }
-  };
+  const [] = useState("Enable light Mode");
 
   return (
-    <div className="container rounded" style={navbg}>
-      <h2 className="pt-3">About Us</h2>
-      <div className="accordion mt-4 " id="accordionExample">
-        <div className="accordion-item">
-          <h2 className="accordion-header">
-            <button
-              className="accordion-button"
-              type="button"
-              style={navbg}
-              data-bs-toggle="collapse"
-              data-bs-target="#collapseOne"
-              aria-expanded="true"
-              aria-controls="collapseOne"
-            >
-              Accordion Item #1
-            </button>
-          </h2>
-          <div
-            id="collapseOne"
-            className="accordion-collapse collapse"
-            data-bs-parent="#accordionExample"
+    <div class="accordion" id="accordionExample">
+      <h1>About Us</h1>
+      <div class="accordion-item mt-4">
+        <h2 class="accordion-header">
+          <button
+            class="accordion-button"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapseOne"
+            aria-expanded="true"
+            aria-controls="collapseOne"
           >
-            <div className="accordion-body" style={navbg}>
-              <strong>This is the first item's accordion body.</strong> It is
-              shown by default, until the collapse plugin adds the appropriate
-              classes that we use to style each element. These classes control
-              the overall appearance, as well as the showing and hiding via CSS
-              transitions. You can modify any of this with custom CSS or
-              overriding our default variables. It's also worth noting that just
-              about any HTML can go within the <code>.accordion-body</code>,
-              though the transition does limit overflow.
-            </div>
-          </div>
-        </div>
-        <div className="accordion-item" style={navbg}>
-          <h2 className="accordion-header">
-            <button
-              className="accordion-button collapsed"
-              style={navbg}
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#collapseThree"
-              aria-expanded="false"
-              aria-controls="collapseThree"
-            >
-              Accordion Item #2
-            </button>
-          </h2>
-          <div
-            id="collapseThree"
-            className="accordion-collapse collapse"
-            data-bs-parent="#accordionExample"
-          >
-            <div className="accordion-body" style={navbg}>
-              <strong>This is the third item's accordion body.</strong> It is
-              hidden by default, until the collapse plugin adds the appropriate
-              classes that we use to style each element. These classes control
-              the overall appearance, as well as the showing and hiding via CSS
-              transitions. You can modify any of this with custom CSS or
-              overriding our default variables. It's also worth noting that just
-              about any HTML can go within the <code>.accordion-body</code>,
-              though the transition does limit overflow.
-            </div>
-          </div>
-        </div>
-        <div className="accordion-item" style={navbg}>
-          <h2 className="accordion-header">
-            <button
-              className="accordion-button collapsed"
-              style={navbg}
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#collapseThree"
-              aria-expanded="false"
-              aria-controls="collapseThree"
-            >
-              Accordion Item #3
-            </button>
-          </h2>
-          <div
-            id="collapseThree"
-            className="accordion-collapse collapse"
-            data-bs-parent="#accordionExample"
-          >
-            <div className="accordion-body" style={navbg}>
-              <strong>This is the third item's accordion body.</strong> It is
-              hidden by default, until the collapse plugin adds the appropriate
-              classes that we use to style each element. These classes control
-              the overall appearance, as well as the showing and hiding via CSS
-              transitions. You can modify any of this with custom CSS or
-              overriding our default variables. It's also worth noting that just
-              about any HTML can go within the <code>.accordion-body</code>,
-              though the transition does limit overflow.
-            </div>
+            Accordion Item #1
+          </button>
+        </h2>
+        <div
+          id="collapseOne"
+          class="accordion-collapse collapse show"
+          data-bs-parent="#accordionExample"
+        >
+          <div class="accordion-body">
+            Welcome to our Text Converter React app – a versatile tool designed
+            to make text transformations easy and convenient. Whether you need
+            to change the case of your text, convert it to uppercase, lowercase,
+            or perform other transformations, our app has you covered.
           </div>
         </div>
       </div>
-
-      <button
-        onClick={togglestyle}
-        type="button"
-        className="btn btn-primary mt-3 mb-3"
-      >
-        {btntext}
-      </button>
+      <div class="accordion-item">
+        <h2 class="accordion-header">
+          <button
+            class="accordion-button collapsed"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapseTwo"
+            aria-expanded="false"
+            aria-controls="collapseTwo"
+          >
+            Accordion Item #2
+          </button>
+        </h2>
+        <div
+          id="collapseTwo"
+          class="accordion-collapse collapse"
+          data-bs-parent="#accordionExample"
+        >
+          <div class="accordion-body">
+            We are a team passionate about creating simple yet powerful tools to
+            enhance your productivity. Our Text Converter React app is just one
+            example of our commitment to providing practical solutions for
+            everyday tasks.
+          </div>
+        </div>
+      </div>
+      <div class="accordion-item">
+        <h2 class="accordion-header">
+          <button
+            class="accordion-button collapsed"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapseThree"
+            aria-expanded="false"
+            aria-controls="collapseThree"
+          >
+            Accordion Item #3
+          </button>
+        </h2>
+        <div
+          id="collapseThree"
+          class="accordion-collapse collapse"
+          data-bs-parent="#accordionExample"
+        >
+          <div class="accordion-body">
+            <strong>Feedback and Support:.</strong> We value your feedback! If
+            you have suggestions, encounter issues, or want to share your
+            thoughts, please reach out to us. Your input helps us improve and
+            tailor our app to meet your needs.
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
